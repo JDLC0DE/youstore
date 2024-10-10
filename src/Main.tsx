c3d539c6adfa8a55e6b748a4359db8fba3ab16d2
Joseph Francisco De La Cruz Rivas <jfdelacruz@avatar-global.com>
2024-10-10 03:18:28 -0500

    feat: add activity indicator

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import { StyleSheet, View, ScrollView, ActivityIndicator } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { CharacterCard } from "./components/CharacterCard";
 
 export default function Main() {
   const [characters, setCharacters] = useState<any[]>([]);
@@ -13,81 +14,31 @@ export default function Main() {
   }, []);
 
   return (
-    <ScrollView
+    <View
       style={{
         ...styles.container,
         paddingTop: insets.top,
         paddingBottom: insets.bottom,
+        justifyContent: characters.length === 0 ? "center" : undefined,
+        alignItems: characters.length === 0 ? "center" : undefined,
       }}
     >
-      <View
-        style={{
-          gap: 20,
-          flex: 1,
-          paddingBottom: 100,
-        }}
-      >
-        {characters.map((character) => (
-          <View key={character.id} style={styles.cardContainer}>
-            <Image source={{ uri: character.image }} style={styles.cardImage} />
-            <View style={styles.cardInfo}>
-              <View style={{ gap: 5 }}>
-                <Text style={styles.cardTitle}>{character.name}</Text>
-                <Text style={styles.cardStatus}>
-                  {character.status} - {character.species}
-                </Text>
-              </View>
-              <View style={{ gap: 5 }}>
-                <Text style={styles.locationTitle}>Last known location:</Text>
-                <Text style={styles.locationValue}>Jerryboree</Text>
-              </View>
-              <View style={{ gap: 5 }}>
-                <Text style={styles.locationTitle}>First seen in:</Text>
-                <Text style={styles.locationValue}>Mortynight Run</Text>
-              </View>
-            </View>
-          </View>
-        ))}
-      </View>
-    </ScrollView>
+      {characters.length > 0 ? (
+        <ScrollView style={{ paddingHorizontal: 30 }}>
+          {characters.map((character) => (
+            <CharacterCard key={character.id} character={character} />
+          ))}
+        </ScrollView>
+      ) : (
+        <ActivityIndicator size="large" />
+      )}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#272b33",
-    paddingHorizontal: 30,
-  },
-  cardContainer: {
-    backgroundColor: "#3c3e44",
-    borderRadius: 10,
-    overflow: "hidden",
-    height: "auto",
-  },
-  cardImage: {
-    width: "100%",
-    height: 320,
-    objectFit: "cover",
-  },
-  cardInfo: {
-    padding: 10,
-    gap: 10,
-  },
-  cardTitle: {
-    color: "#fff",
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  cardStatus: {
-    color: "#fff",
-    fontSize: 16,
-  },
-  locationTitle: {
-    color: "#75929e",
-    fontSize: 16,
-  },
-  locationValue: {
-    color: "#fff",
-    fontSize: 16,
+    flex: 1,
   },
 });
---
+++ src/components/CharacterCard.tsx
@@ -0,0 +1,63 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+
+export const CharacterCard = ({ character }: { character: any }) => {
+  return (
+    <View style={styles.cardContainer}>
+      <Image source={{ uri: character.image }} style={styles.cardImage} />
+      <View style={styles.cardInfo}>
+        <View style={{ gap: 5 }}>
+          <Text style={styles.cardTitle}>{character.name}</Text>
+          <Text style={styles.cardStatus}>
+            {character.status} - {character.species}
+          </Text>
+        </View>
+        <View style={{ gap: 5 }}>
+          <Text style={styles.locationTitle}>Last known location:</Text>
+          <Text style={styles.locationValue}>Jerryboree</Text>
+        </View>
+        <View style={{ gap: 5 }}>
+          <Text style={styles.locationTitle}>First seen in:</Text>
+          <Text style={styles.locationValue}>Mortynight Run</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#272b33",
+  },
+  cardContainer: {
+    backgroundColor: "#3c3e44",
+    borderRadius: 10,
+    overflow: "hidden",
+    height: "auto",
+  },
+  cardImage: {
+    width: "100%",
+    height: 320,
+    objectFit: "cover",
+  },
+  cardInfo: {
+    padding: 10,
+    gap: 10,
+  },
+  cardTitle: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  cardStatus: {
+    color: "#fff",
+    fontSize: 16,
+  },
+  locationTitle: {
+    color: "#75929e",
+    fontSize: 16,
+  },
+  locationValue: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
